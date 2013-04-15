289a4f3d7b7fe550b29ad7a4a2521c169a075125
Jeremy Stanley <jeremy@obvious.com>
2013-04-15 14:11:15 -0700

    Context stuff that was never merged into master for some reason

--- kew.js
+++ kew.js
@@ -12,6 +12,24 @@ function Promise(onSuccess, onFail) {
   this._isPromise = true
   this._successFn = onSuccess
   this._failFn = onFail
+  this._context = undefined
+}
+
+/**
+ * Set the context for a promise
+ * @param {context} context An arbitrary context
+ */
+Promise.prototype.setContext = function (context) {
+  this._context = context
+  return this
+}
+
+/**
+ * Get the context for a promise
+ * @return {Object} the context set by setContext
+ */
+Promise.prototype.getContext = function () {
+  return this._context
 }
 
 /**
@@ -101,6 +119,7 @@ Promise.prototype.reject = function (e) {
  */
 Promise.prototype.then = function (onSuccess, onFail) {
   var promise = new Promise(onSuccess, onFail)
+  if (this._context) promise.setContext(this._context)
 
   if (this._child) this._child._chainPromise(promise)
   else this._chainPromise(promise)
@@ -163,7 +182,7 @@ Promise.prototype.end = function () {
 Promise.prototype._withInput = function (data) {
   if (this._successFn) {
     try {
-      this.resolve(this._successFn(data))
+      this.resolve(this._successFn(data, this._context))
     } catch (e) {
       this.reject(e)
     }
@@ -178,7 +197,7 @@ Promise.prototype._withInput = function (data) {
 Promise.prototype._withError = function (e) {
   if (this._failFn) {
     try {
-      this.resolve(this._failFn(e))
+      this.resolve(this._failFn(e, this._context))
     } catch (e) {
       this.reject(e)
     }
