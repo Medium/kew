9ad3424f51cc6dafa29d9f26a10f9b40984dc0e8
Nick Santos <nick@medium.com>
2015-05-13 18:55:25 -0400

    add spread() to kew

---
+++ test/spread.js
@@ -0,0 +1,36 @@
+var Q = require('../kew')
+var originalQ = require('q')
+
+exports.testSpreadStatic = function (test) {
+  Q.spread([Q.resolve('a'), 'b'], function (a, b) {
+    test.equal('a', a)
+    test.equal('b', b)
+    test.done()
+  })
+}
+
+exports.testSpreadMethod = function (test) {
+  Q.resolve(true)
+      .then(function () {
+        return ['a', 'b']
+      })
+      .spread(function (a, b) {
+        test.equal('a', a)
+        test.equal('b', b)
+        test.done()
+      })
+}
+
+exports.testSpreadBoundMethod = function (test) {
+  Q.resolve(true)
+      .then(function () {
+        return [Q.resolve('a'), 'b']
+      })
+      .spreadBound(function (c, a, b) {
+        test.equal('scope', this.scope)
+        test.equal('c', c)
+        test.equal('a', a)
+        test.equal('b', b)
+        test.done()
+      }, {scope: 'scope'}, 'c')
+}
